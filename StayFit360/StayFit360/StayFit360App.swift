05e3ee0a73ca0b2ef7c97349012d9d1ca58b0985
Pratik Ghadge <pratikhg.2001@gmail.com>
2025-10-01 23:24:55 +0530

    chore: initial project setup for StayFit360 iOS app

---
+++ StayFit360/StayFit360/StayFit360App.swift
@@ -0,0 +1,17 @@
+//
+//  StayFit360App.swift
+//  StayFit360
+//
+//  Created by Pratik Haridas Ghadge on 01/10/25.
+//
+
+import SwiftUI
+
+@main
+struct StayFit360App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
