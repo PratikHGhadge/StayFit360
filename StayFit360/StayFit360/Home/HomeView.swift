400fc267b58850bf6ab395b3b7275ed1400e518e
Pratik Ghadge <pratikhg.2001@gmail.com>
2025-10-02 21:35:35 +0530

    feat: add ActivityCard component to display fitness activity data

--- StayFit360/StayFit360/Home/HomeView.swift
+++ StayFit360/StayFit360/Home/HomeView.swift
@@ -12,10 +12,44 @@ struct HomeView: View {
 	@State var activeMinutes: Int = 52
 	@State var standHours: Int = 8
 
+	var mockActivities: [Activity] = [
+		Activity(
+			id: 0,
+			title: "Today Steps",
+			subtitle: "Goal 12,000",
+			image: "figure.walk",
+			tintColor: .green,
+			amount: "9,812"
+		),
+		Activity(
+			id: 1,
+			title: "Calories Burned",
+			subtitle: "Goal 1,000",
+			image: "flame.fill",
+			tintColor: .red,
+			amount: "812"
+		),
+		Activity(
+			id: 2,
+			title: "Distance",
+			subtitle: "Goal 12 km",
+			image: "figure.walk",
+			tintColor: .blue,
+			amount: "9.8 km"
+		),
+		Activity(
+			id: 3,
+			title: "Running Steps",
+			subtitle: "Goal 50,000",
+			image: "figure.run",
+			tintColor: .purple,
+			amount: "55,812"
+		)
+	]
 
 	var body: some View {
 		ScrollView(showsIndicators: false) {
-			VStack {
+			VStack(alignment: .leading) {
 				Text("Welcome")
 					.font(.largeTitle)
 					.padding()
@@ -85,6 +119,32 @@ struct HomeView: View {
 					Spacer()
 				}
 				.padding(.horizontal)
+
+				HStack {
+					Text("See all activity")
+						.font(.title2)
+					Spacer()
+
+					Button {
+						print("Button Tapped")
+					} label: {
+						Text("Show more")
+							.padding(.all, 10)
+							.foregroundColor(.white)
+							.background(.blue)
+							.cornerRadius(20)
+					}
+				}
+				.padding(.horizontal)
+
+				LazyVGrid(columns: Array(
+					repeating: GridItem(.flexible()),
+					count: 2)) {
+						ForEach(mockActivities, id: \.id) { activity in
+							ActivityCard(activity: activity)
+						}
+					}
+					.padding(.horizontal)
 			}
 		}
 	}
